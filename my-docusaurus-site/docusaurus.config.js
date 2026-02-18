4f3e21064a413ad66a1f6a098c9df0801a7c9a80
abdulahi hasan <abdulahi3432@gmail.com>
2026-02-18 11:12:08 +0000

    initial doploy

--- my-docusaurus-site/docusaurus.config.js
+++ my-docusaurus-site/docusaurus.config.js
@@ -27,10 +27,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'abood-334', // Usually your GitHub org/user name.
+  projectName: 'abdulahi', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'throw'
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
